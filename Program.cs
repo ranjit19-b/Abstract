ebeb5cf118d85a170d624951accc64a6d16ef8ad
ranjit19-b <ranjit1041997kumar@gmail.com>
2024-10-14 19:18:22 +0530

    added

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Abstract
+{
+    abstract class Animal  //Abstract class
+    {
+        //Abstract method (does have a body)
+        public abstract void animalSound();
+
+        //Regular method
+        public void sleep()
+        {
+            Console.WriteLine("Zzz");
+        }
+    }
+
+    class Pig : Animal  //Derived class (inherit from Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("This pig says: wee wee");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Pig myPig = new Pig();  //create a pig object
+            myPig.animalSound();    //call the abstract method
+            myPig.sleep();          //call the regular method
+        }
+    }
+}
